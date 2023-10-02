151557e60d63e9c463306f75476b23effcd7018a
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2023-10-02 20:00:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
+
